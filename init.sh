273054dcfbb66abdb96fe6bf905bf2786f32c669
Linjinkun <380993970@qq.com>
2023-05-04 17:22:54 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,9 +16,7 @@ sudo systemctl restart docker
 
 
 # 安装docker-compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo curl -L "https://mirrors.aliyun.com/docker-toolbox/linux/compose/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
