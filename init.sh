8e6ff832453ca09c409ea674640d4ac5eb338fae
Linjinkun <380993970@qq.com>
2023-05-04 16:38:54 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 添加阿里云镜像加速器
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://your-aliyun-mirror.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+# 安装docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# 安装docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo curl -L "https://mirrors.aliyun.com/docker-toolbox/linux/compose/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+# 输出docker和docker-compose版本信息
+echo "Docker version:"
+docker --version
+echo ""
+echo "Docker-compose version:"
+docker-compose --version
